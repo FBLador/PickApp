b3cac5e66322c112992c0a154e2baac5941419a0
FBLador <flaviusb.lador@gmail.com>
2022-02-13 18:54:14 +0100

    Added recyclerView in homeFragment

--- app/src/main/java/it/unimib/pickapp/ui/homeFragment.java
+++ app/src/main/java/it/unimib/pickapp/ui/homeFragment.java
@@ -2,14 +2,24 @@ package it.unimib.pickapp.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import it.unimib.pickapp.R;
+import it.unimib.pickapp.model.Match;
 
 /**
  * It shows the homepage of the app.
@@ -18,6 +28,10 @@ public class homeFragment extends Fragment {
 
     private TextView titleToolbar;
     private final String title = "Home";
+    private RecyclerView recyclerView;
+    DatabaseReference mbase;
+    private static final String TAG = "homeFragment";
+    matchesAdapter adapter; // Create Object of the Adapter class
 
     public homeFragment() {
         // Required empty public constructor
@@ -30,12 +44,65 @@ public class homeFragment extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
 
         setTitle(view);
+        recyclerView = view.findViewById(R.id.recycler_view_games);
+
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(getActivity()));
+
+        // Create a instance of the database and get
+        // its reference
+        mbase = FirebaseDatabase.getInstance().getReference("Matches");
+
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        FirebaseRecyclerOptions<Match> options
+                = new FirebaseRecyclerOptions.Builder<Match>()
+                .setQuery(mbase, Match.class)
+                .build();
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = new matchesAdapter(options);
+        // Connecting Adapter class with the Recycler view
+        recyclerView.setAdapter(adapter);
 
         return view;
+    }
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
+
+        mbase = FirebaseDatabase.getInstance().getReference("Matches");
+
+        // TODO
+        //.orderByChild("dateTime").equalTo(dayOfMonth + "/" + month + "/" + year)
+        FirebaseRecyclerOptions<Match> options
+                = new FirebaseRecyclerOptions.Builder<Match>()
+                .setQuery(mbase, Match.class)
+                .build();
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = new matchesAdapter(options);
+        // Connecting Adapter class with the Recycler view*/
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+        recyclerView.invalidate();
     }
 
     public void setTitle(View view){
         titleToolbar = view.findViewById(R.id.titleHome);
         titleToolbar.setText(title);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
