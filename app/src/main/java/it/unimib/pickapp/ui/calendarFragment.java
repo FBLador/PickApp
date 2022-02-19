143b9f6615f4ccec76065b8e677514ab2ad86624
Elia <eliasantobuono@gmail.com>
2022-02-19 17:23:33 +0100

    Calendar filter and show/hide button

--- app/src/main/java/it/unimib/pickapp/ui/calendarFragment.java
+++ app/src/main/java/it/unimib/pickapp/ui/calendarFragment.java
@@ -9,9 +9,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,14 +26,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.Query;
 
+import java.util.Calendar;
 import java.util.Objects;
+import java.util.TimeZone;
 
 import it.unimib.pickapp.R;
 import it.unimib.pickapp.model.Match;
@@ -42,16 +41,16 @@ public class calendarFragment extends Fragment {
 
     private CalendarView calendarView;
     private RecyclerView recyclerView;
+    private View HideShowView;
     private FloatingActionButton addButton;
+    private Button hideShowButton;
     private DatabaseReference mbase;
     private DatabaseReference locationReference;
-
+    private String selectedDate;
+    private Query query;
+    boolean flag=true;
     private static final String TAG = "calendarFragment";
-    private TextView titolo, luogo, numeroSquadre, data, durata, costo, sport, descrizione;
-
-    matchesAdapter adapter; // Create Object of the Adapter class
-    DatabaseReference mDatabase; // Create object of the Firebase Realtime Database
-
+    private matchesAdapter adapter; // Create Object of the Adapter class
     private matchesAdapter.ItemClickListener itemClickListener;
 
     public calendarFragment() {
@@ -76,42 +75,70 @@ public class calendarFragment extends Fragment {
         ((pickappActivity) getActivity()).setSupportActionBar(toolbar);
 
         calendarView = rootView.findViewById(R.id.calendarView);
+        HideShowView = rootView.findViewById(R.id.linearLayout2);
         addButton = rootView.findViewById(R.id.create_game);
+        hideShowButton = rootView.findViewById(R.id.HideShow);
 
+        hideShowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId())
+                {
+                    case R.id.HideShow:
+                        if(flag)
+                        {
+                            HideShowView.setVisibility(View.GONE);
+                            flag=false;
+                        }
+                        else {
+                            HideShowView.setVisibility(View.VISIBLE);
+                            flag=true;
+                        }
+                }
+            }
+        });
         recyclerView = rootView.findViewById(R.id.recycler_view_games);
 
         // To display the Recycler view linearly
         recyclerView.setLayoutManager(
                 new LinearLayoutManager(getActivity()));
 
-        // Create a instance of the database and get
-        // its reference
+
+        // Choose time zone in which you want to interpret your Date
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"));
+
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int month = cal.get(Calendar.MONTH);
+        int year = cal.get(Calendar.YEAR);
+
+        selectedDate = day + "/"
+                + (month+1) + "/"
+                + year;
+
         mbase = FirebaseDatabase.getInstance().getReference("Matches");
+        query = mbase.orderByChild("date").equalTo(selectedDate);
         locationReference = FirebaseDatabase.getInstance().getReference("Places");
 
-
-        // It is a class provide by the FirebaseUI to make a
-        // query in the database to fetch appropriate data
         FirebaseRecyclerOptions<Match> options
                 = new FirebaseRecyclerOptions.Builder<Match>()
-                .setQuery(mbase, Match.class)
+                .setQuery(query, Match.class)
                 .build();
 
-        itemClickListener = match -> {
+        matchesAdapter.ItemClickListener itemClickListener = match -> {
             MatchViewModel matchViewModel =
                     new ViewModelProvider(requireActivity()).get(MatchViewModel.class);
 
             matchViewModel.setMatch(match);
             NavController navController = NavHostFragment.findNavController(this);
-            navController.navigate(R.id.action_calendar_to_match);
+            navController.navigate(R.id.action_home_to_match);
         };
-
         // Connecting object of required Adapter class to
         // the Adapter class itself
         adapter = new matchesAdapter(options, itemClickListener, locationReference);
-        // Connecting Adapter class with the Recycler view
+        // Connecting Adapter class with the Recycler view*/
         recyclerView.setAdapter(adapter);
-
+        adapter.startListening();
+        recyclerView.invalidate();
 
         return rootView;
     }
@@ -119,77 +146,35 @@ public class calendarFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
 
-
-
-        /*
-        List<Match> matchList = new ArrayList<>();
-        mbase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                matchList.clear();
-                for (DataSnapshot postSnapshot: snapshot.getChildren()) {
-                    Match match = postSnapshot.getValue(Match.class);
-                    matchList.add(match);
-
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getMessage());
-            }
-        });
-
-
-        System.out.println(matchList); */
-
-        //updateRecycler();
         calendarView.setOnDateChangeListener((view1, year, month, dayOfMonth) -> {
-            Toast.makeText(getActivity(), "Date changed to " + dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getActivity(), "Date changed to " + dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_SHORT).show();
             Log.i(TAG, "Date changed to " + dayOfMonth + "/" + month + "/" + year);
 
+            selectedDate = dayOfMonth + "/" + (month+1) + "/" + year;
             mbase = FirebaseDatabase.getInstance().getReference("Matches");
+            query = mbase.orderByChild("date").equalTo(selectedDate);
+            locationReference = FirebaseDatabase.getInstance().getReference("Places");
 
-            // TODO
-            //.orderByChild("dateTime").equalTo(dayOfMonth + "/" + month + "/" + year)
             FirebaseRecyclerOptions<Match> options
                     = new FirebaseRecyclerOptions.Builder<Match>()
-                    .setQuery(mbase, Match.class)
+                    .setQuery(query, Match.class)
                     .build();
+
+            matchesAdapter.ItemClickListener itemClickListener = match -> {
+                MatchViewModel matchViewModel =
+                        new ViewModelProvider(requireActivity()).get(MatchViewModel.class);
+
+                matchViewModel.setMatch(match);
+                NavController navController = NavHostFragment.findNavController(this);
+                navController.navigate(R.id.action_home_to_match);
+            };
             // Connecting object of required Adapter class to
             // the Adapter class itself
-
             adapter = new matchesAdapter(options, itemClickListener, locationReference);
             // Connecting Adapter class with the Recycler view*/
             recyclerView.setAdapter(adapter);
             adapter.startListening();
             recyclerView.invalidate();
-
-           /*
-            // Create a instance of the database and get
-            // its reference
-            mbase = FirebaseDatabase.getInstance().getReference("Matches");
-
-            recyclerView = getActivity().findViewById(R.id.recycler_view_games);
-
-            // To display the Recycler view linearly
-            recyclerView.setLayoutManager(
-                    new LinearLayoutManager(getContext()));
-
-
-            // It is a class provide by the FirebaseUI to make a
-            // query in the database to fetch appropriate data
-            FirebaseRecyclerOptions<Match> options
-                    = new FirebaseRecyclerOptions.Builder<Match>()
-                    .setQuery(mbase, Match.class)
-                    .build();
-            // Connecting object of required Adapter class to
-            // the Adapter class itself
-            adapter = new matchesAdapter(options);
-
-            // Connecting Adapter class with the Recycler view
-            recyclerView.setAdapter(adapter);*/
-
         });
 
         addButton.setOnClickListener(v -> {
@@ -200,60 +185,31 @@ public class calendarFragment extends Fragment {
             NavController navController = NavHostFragment.findNavController(this);
             navController.navigate(R.id.action_calendar_to_match);
         });
-
-
-    }
-
-    public void updateRecycler() { //Aggiorna la recycle view con le partite del giorno selezionato
-        //get info from firebase
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Matches");
-        String userID = Objects.requireNonNull(user).getUid();
-        Log.d(TAG, userID);
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                for (DataSnapshot child : snapshot.getChildren()) {
-                    String titoloPartita = child.child("titolo").getValue(String.class);
-
-                    titolo.setText(titoloPartita);
-                }
-
-
-                // Log.d(TAG, nick_name);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
 
     @Override
-        public void onStart() {
-            super.onStart();
-            adapter.startListening();
-        }
-
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
 
     @Override
-        public void onStop() {
-            super.onStop();
-            adapter.stopListening();
-        }
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
 
-    public void setTitle(View view, String title){
+    public void setTitle(View view, String titleCal){
         TextView titleToolbar = view.findViewById(R.id.titleCalendar);
-        titleToolbar.setText(title);
+        titleToolbar.setText(titleCal);
     }
+
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         // The custom menu that we want to add to the toolbar
         inflater.inflate(R.menu.logout_menu, menu);
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
@@ -265,5 +221,4 @@ public class calendarFragment extends Fragment {
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
